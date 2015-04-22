fa6b74bb8185683a11555cf34454d8d43869f047
JohnDuxbury <JohnDuxbury@users.noreply.github.com>
2015-04-22 16:53:49 +0100

    Mailer Update

--- Chapter 7 PHP/index.php
+++ Chapter 7 PHP/index.php
@@ -1,23 +1,6 @@
 <?php
 
-	echo "This is PHP!";
-	
-	$test="I am a variable";
-	
-	echo $test;
-	
-?> am a variable too. ";
-	
-	$number=75/3;
-	
-	$name="Rob";
-	
-	$a="name";
-	
-	echo $test." ".$test2." ".$number." ";
-	
-	echo "My name is $name ";
-	
-	echo $$a
+	echo "This is PHP! ";
+
 	
 ?>
---
+++ Chapter 7 PHP/while.php
@@ -0,0 +1,31 @@
+<?php
+
+	echo "This is a 'while' loop using a variable equal to 1, with a ++ increment<br/>";
+
+	$i=1;
+	
+	while($i<20){
+	
+	echo $i." ";
+	
+	$i++;
+	
+	}
+	
+	echo "<br/><br/>";
+	
+	echo "A while loop running through an array until all array items displayed!<br/>";
+	$array=array("apple","pear","custard");
+
+	$j=0;
+
+	while($array[$j]){
+	
+	//echo $array[$j]." ";
+	echo "Key: $j Value: $array[$j] <br/>";
+	
+	$j++;
+	
+	}
+
+?>

944276dc677777121ac4c353e7d7ce8ec7c1de68
JohnDuxbury <JohnDuxbury@users.noreply.github.com>
2015-04-30 12:52:51 +0100

    Folder Re-Format and Update

--- Chapter 7 PHP/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-	echo "This is PHP! <br/><br/>";
-	
- 	//use a \ to escape " from text
-
-	echo "<a href=\"index.html\">Index HTML</a>";
-?>
--- Chapter 8 MySQL/cookie.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-	//setcookie("id","9876",time()+3600);
-	
-	setcookie("id","9876",time()-3600);
-	
-		echo $_COOKIE["id"];
-
-?>
